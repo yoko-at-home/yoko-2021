fdb88b359d6dad6440a78eb76fb04819a8beb48b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-08 10:16:16 +0900

    🚨 lintエラーの修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,115 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    ecmaVersion: 2021,
+    sourceType: "module",
+    project: ["tsconfig.json"],
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "import",
+    "simple-import-sort",
+    "react-hooks",
+    "tailwindcss",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+    "next",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "no-console": ["error", { allow: ["warn", "info", "error"] }],
+    "no-restricted-syntax": [
+      "error",
+      { selector: "TSEnumDeclaration", message: "Don't declare enums" },
     ],
-    "rules": {
-    }
+    "prefer-arrow-callback": "error",
+    "func-style": ["error", "expression"],
+    "arrow-body-style": ["error", "always"],
+    "no-restricted-imports": [
+      "error",
+      { paths: [{ name: "react", importNames: ["default"] }] },
+    ],
+    "react/display-name": "error",
+    "react/jsx-handler-names": [
+      "error",
+      {
+        eventHandlerPrefix: "handle",
+        eventHandlerPropPrefix: "on",
+        checkLocalVariables: true,
+        checkInlineFunction: true,
+      },
+    ],
+    "react/destructuring-assignment": ["error", "never"],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/newline-after-import": "error",
+    "import/no-default-export": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { prefer: "type-imports" },
+    ],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
+      {
+        selector: ["property", "parameterProperty", "method"],
+        format: ["camelCase"],
+      },
+      {
+        selector: "variable",
+        types: ["boolean"],
+        format: ["PascalCase"],
+        prefix: ["is", "has", "should"],
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+    "tailwindcss/classnames-order": "warn",
+    "tailwindcss/no-custom-classname": "warn",
+    "tailwindcss/no-contradicting-classname": "error",
+  },
+  overrides: [
+    {
+      files: ["src/pages/**/*.{js,jsx,tsx}"],
+      rules: { "import/no-default-export": "off" },
+      parserOptions: {
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
+      },
+    },
+  ],
 };
