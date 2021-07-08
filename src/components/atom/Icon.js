12f0160fddf536f3d1a943adf948b32fd69b2794
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-08 13:18:58 +0900

    ✨ fontawesomeのインストールとアイコンの作成

---
+++ src/components/atom/Icon.js
@@ -0,0 +1,54 @@
+export const Icons = () => {
+  return (
+    <div className='mt-6 lg:mb-0 mb-6'>
+      <a
+        href='https://twitter.com/yokoiwasaki6'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <button
+          className='hover:bg-indigo-200 hover:text-white bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+          type='button'
+        >
+          <i className='fab fa-twitter-square'></i>
+        </button>
+      </a>
+      <a
+        href='https://www.linkedin.com/in/yoko-iwasaki-31b505112/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <button
+          className='hover:bg-indigo-200 hover:text-white bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+          type='button'
+        >
+          <i className='fab fa-linkedin'></i>
+        </button>
+      </a>
+      <a
+        href='https://github.com/yoko-at-home'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <button
+          className='hover:bg-indigo-200 hover:text-white bg-white text-black shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+          type='button'
+        >
+          <i className='fab fa-github'></i>
+        </button>
+      </a>
+      <a
+        href='https://www.instagram.com/yoko_123123/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <button
+          className='hover:bg-indigo-200 hover:text-white bg-white text-red-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+          type='button'
+        >
+          <i className='fab fa-instagram-square'></i>
+        </button>
+      </a>
+    </div>
+  );
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
