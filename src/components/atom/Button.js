ce3c0f8888a51524a5d1e3fb64ceacb32b539be8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-08 20:46:40 +0900

    ✨ Bottonコンポーネント作成

---
+++ src/components/atom/Button.js
@@ -0,0 +1,14 @@
+export const Button = (props) => {
+  return (
+    <div>
+      <a
+        href='#_'
+        className='group relative py-3 px-6 font-bold text-white rounded-lg'
+      >
+        <span className='absolute inset-0 w-full h-full bg-blue-800 opacity-80 transition duration-300 ease transform -translate-x-1 group-hover:translate-x-0 -translate-y-1 group-hover:translate-y-0'></span>
+        <span className='absolute inset-0 w-full h-full bg-purple-800 opacity-80 mix-blend-screen transition duration-300 ease transform translate-x-1 group-hover:translate-x-0 translate-y-1 group-hover:translate-y-0'></span>
+        <span className='relative'>{props.name}</span>
+      </a>
+    </div>
+  );
+};
