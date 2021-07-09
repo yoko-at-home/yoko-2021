bbd12d4372d021c96add227053d2c1fff1a9286e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-09 10:48:11 +0900

    Headの内容更新

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,24 +1,33 @@
 import Head from "next/head";
 
-export const HeadPart = (title, description, keyword, image, url) => {
+export const HeadPart = () => {
+  const image = "/banner.jpg"
   return (
     <Head>
-      <title>yoko | プログラミングを学ぶ</title>
-      {/* <title>{title}</title> */}
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={description} />
-      <meta name='keywords' content={keyword} />
+      <title>yoko | webで遊ぶ | プログラミングを学ぶ</title>
+      <meta property='og:title' content='yoko Webで遊ぶ' />
+      <meta
+        property='og:description'
+        content='2020年から2021年の活動をまとめました'
+      />
+      <meta name='keywords' content='プログラミング, Web制作' />
       <meta property='og:type' content='blog' />
-      <meta property='og:url' content={url} />
+      <meta property='og:url' content='https://yoko-2021.vercel.app/' />
       <meta property='og:image' content={image} />
-      <meta property='og:site_name' content={title} />
-      <meta name='twitter:card' content='summary' />
+      <meta property='og:site_name' content='yoko Webで遊ぶ' />
+      <meta
+        name='twitter:card'
+        content='2020年から2021年の活動をまとめました'
+      />
       <meta name='twitter:site' content='@yokoiwasaki8' />
       <meta name='twitter:url' content={image} />
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
+      <meta name='twitter:title' content='yoko Webで遊ぶ' />
+      <meta
+        name='twitter:description'
+        content='2020年から2021年の活動をまとめました'
+      />
       <meta name='twitter:image' content={image} />
-      <link rel='canonical' href={url} />
+      <link rel='canonical' href='https://yoko-2021.vercel.app/' />
       <link rel='shortcut icon' href={"/favicon.png"} />
       {/* <link rel='apple-touch-icon' href={"https://t-cr.jp/logo.png"} /> */}
       <link
