122830dcd4b37fd11362ac5ecda3bac9ce135a5f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-09 10:33:50 +0900

    ✨ ぎゅっと縮む画像処理

---
+++ src/components/atom/ImageShrink.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export const ImageShrink = (src, alt, href) => {
+  return (
+    <div className='relative'>
+      <div className='absolute top-5 left-5 z-50'>
+        <span role='img' aria-label='sparkles'>
+          ✨
+        </span>
+      </div>
+      <Image
+        className='md:min-w-full hover:opacity-90 transition duration-500 ease-in-out transform hover:scale-95'
+        width={700}
+        height={500}
+        src={src}
+        alt={alt}
+        href={href}
+      />
+    </div>
+  );
+};
