34dd441fe2be152dd5f683d33904ceac30edbb32
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-09 17:59:35 +0900

    programmingページ内容追加

---
+++ src/pages/programming.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Layout } from "../components/Layout";
+
+const programming = () => {
+  return (
+    <Layout>
+      プログラミングページの作成
+    </Layout>
+  );
+};
+
+export default programming;
