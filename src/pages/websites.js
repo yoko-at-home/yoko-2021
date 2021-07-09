35f0e80c05fb27d86d57d50df404f119a00064ad
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-09 11:50:54 +0900

    ✨ Websiteページ作成

---
+++ src/pages/websites.js
@@ -0,0 +1,11 @@
+import { Layout } from "../components/Layout";
+
+const websites = () => {
+  return (
+    <Layout>
+      <div>websites</div>;
+    </Layout>
+  );
+};
+
+export default websites;
