b4ca549471515dd24b9c1606829f4bbf09a7e03e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-10 09:37:23 +0900

    トップへ遷移の✨追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 import { FireworksSection } from "../components/Fireworks";
 import { HeadPart } from "../components/Head";
 import { Header } from "../components/Header";
 import { Layout } from "../components/Layout";
 import { Tiles } from "../components/Tiles";
 
-const Home=()=> {
+const Home = () => {
   return (
     <Layout>
       <HeadPart
@@ -14,11 +16,25 @@ const Home=()=> {
         image={"/banner.jpg"}
         url={"https://yoko-2021.vercel.app"}
       />
-      <Header />
-        <FireworksSection />
+      <Header id="top"/>
+      <FireworksSection />
       <Tiles />
+      <div className='mt-5 text-center'>
+        <p>
+          <Link href='/#top'>
+            <a
+              className='py-2 mr-4 text-sm font-bold leading-relaxed uppercase whitespace-nowrap '
+              href='#pablo'
+            >
+              <span role='img' aria-label='sparkles'>
+                ✨
+              </span>
+            </a>
+          </Link>
+        </p>
+      </div>
     </Layout>
   );
-}
+};
 
 export default Home;
