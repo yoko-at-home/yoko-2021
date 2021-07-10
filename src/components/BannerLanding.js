4d122ac6a9e73dadcbf57b292a78f2e9032d4a7d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-10 09:15:29 +0900

    fine tuning

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -2,18 +2,20 @@ export const BannerLanding = (props) => {
   return (
     <section
       id='banner'
-      className='p-20 h-full text-4xl text-yellow-100 bg-center bg-cover opacity-50'
+      className='p-20 h-full text-4xl text-purple-100 bg-center bg-cover opacity-60'
       style={{
         backgroundColor: "#8d82c4",
         backgroundImage: "url('/kota.jpg')",
       }}
     >
-      <div className='block my-0 mx-auto opacity-100'>
-        <h1 className='mb-5 text-3xl'>{props.title}</h1>
-        <p className='text-2xl animate-pulse'>
+      <div className='inline-flex flex-col my-0 mx-auto'>
+        <span className='inline-block p-1 mb-5 text-4xl font-extrabold text-center bg-purple-900 bg-opacity-50 rounded animate-pulse'>
+          {props.title}
+        </span>
+        <span className='inline-block p-1 text-2xl font-black text-center bg-purple-900 bg-opacity-50 rounded animate-pulse'>
           {props.description}
           <br />
-        </p>
+        </span>
       </div>
     </section>
   );
