d1d419486f995a6b12dddcb943c0d5f284b1fe9e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-12 10:13:43 +0900

    SEO

--- src/components/atom/Button.tsx
+++ src/components/atom/Button.tsx
@@ -10,7 +10,7 @@ type InputType = {
 
 export const Button: VFC<InputType> = (props) => {
   return (
-    <div>
+    <button aria-label="Move to Link">
       <a
         href={props.src}
         className='group relative py-3 px-6 font-bold text-white rounded-lg'
@@ -19,12 +19,12 @@ export const Button: VFC<InputType> = (props) => {
         <span className='absolute inset-0 w-full h-full bg-purple-800 opacity-80 mix-blend-screen transition duration-300 ease transform translate-x-1 group-hover:translate-x-0 translate-y-1 group-hover:translate-y-0'></span>
         <span className='relative'>{props.children}</span>
       </a>
-    </div>
+    </button>
   );
 };
 export const Button2: VFC<InputType> = (props) => {
   return (
-    <div>
+    <button aria-label="Move to Link">
       <a
         href={props.src}
         className='group relative py-3 px-6 font-bold text-white rounded-lg'
@@ -33,12 +33,12 @@ export const Button2: VFC<InputType> = (props) => {
         <span className='absolute inset-0 w-full h-full bg-yellow-900 shadow-2xl opacity-70 mix-blend-screen transition duration-300 ease transform translate-x-1 group-hover:translate-x-0 translate-y-1 group-hover:translate-y-0'></span>
         <span className='relative'>{props.children}</span>
       </a>
-    </div>
+    </button>
   );
 };
 export const ButtonYellow: VFC<InputType> = (props) => {
   return (
-    <div>
+    <button aria-label="Move to Link">
       <a
         href={props.src}
         className='group relative py-3 px-6 font-bold text-white rounded-lg'
@@ -47,6 +47,6 @@ export const ButtonYellow: VFC<InputType> = (props) => {
         <span className='absolute inset-0 w-full h-full bg-yellow-200 shadow-2xl opacity-70 mix-blend-screen transition duration-300 ease transform translate-x-1 group-hover:translate-x-0 translate-y-1 group-hover:translate-y-0'></span>
         <span className='relative text-blue-800'>{props.children}</span>
       </a>
-    </div>
+    </button>
   );
 };
