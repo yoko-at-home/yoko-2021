80bdcaea8aa5b5a00885283bc92a0304307ead9f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-13 13:12:21 +0900

    🚨

--- src/components/KotaroAnimation.js
+++ src/components/KotaroAnimation.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const KotaroAnimation = () => {
   return (
     <div
-      className='flex md:hidden fixed top-1/3 left-1/4 flex-col uppercase justify-start p-2 leading-tight text-center text-purple-400 rounded-full border-8 border-pink-400 border-dotted animate-ping'
+      className='flex md:hidden fixed top-1/3 left-1/4 flex-col justify-start p-2 leading-tight text-center text-purple-400 uppercase rounded-full border-8 border-pink-400 border-dotted animate-ping'
       style={{ fontFamily: "cursive", fontSize: "4px" }}
     >
       l
