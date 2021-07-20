18d4281e83d21412e83e0f2a394b41720616304a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-20 17:39:40 +0900

    💄 paddingの調整

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -2,13 +2,13 @@ export const BannerLanding = (props) => {
   return (
     <section
       id='banner'
-      className='p-20 h-full text-4xl text-purple-100 bg-center bg-cover opacity-60'
+      className='md:p-20 py-10 px-5 h-full text-4xl text-purple-100 bg-center bg-cover opacity-60'
       style={{
         backgroundColor: "#8d82c4",
         backgroundImage: "url('/kota.jpg')",
       }}
     >
-      <div className='inline-flex p-10 bg-clip-padding bg-opacity-60 md:border md:border-gray-300 md:backdrop-filter md:backdrop-blur-xl'>
+      <div className='inline-flex p-10 md:w-1/3 bg-clip-padding bg-opacity-60 md:border md:border-gray-300 md:backdrop-filter md:backdrop-blur-xl'>
         <div className='inline-flex flex-col my-0 mx-auto'>
           <span className='inline-block p-1 mb-5 text-4xl font-extrabold text-transparent bg-clip-text bg-opacity-90 bg-gradient-to-r from-green-300 to-blue-600'>
             {props.title}
