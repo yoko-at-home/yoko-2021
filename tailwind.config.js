06944bec11633bc226929c055d33dd61081f7b77
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-09-12 22:44:52 +0900

    ✨ Kaisei opti導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Kaisei opti", "Open Sans", "Arial", "sans-serif"],
+    },
     extend: {},
   },
   variants: {
