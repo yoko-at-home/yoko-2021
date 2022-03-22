dded3bda3546bf017214a8d6046b436a3de2e22b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-22 22:01:53 +0900

    clubhouseお休みを明記

--- src/pages/language.js
+++ src/pages/language.js
@@ -9,23 +9,25 @@ const language = () => {
   return (
     <Layout>
       <BannerLanding
-        title='産業翻訳者やってました'
-        description='翻訳など過去の職歴については、フッターのリンクから、LinkedInをご参照ください。'
+        title="産業翻訳者やってました"
+        description="翻訳など過去の職歴については、フッターのリンクから、LinkedInをご参照ください。"
       />
-      <div className='grid grid-cols-1 gap-5'>
+      <div className="grid grid-cols-1 gap-5">
         {/* <div className='absolute top-5 left-5 z-50' desc={"desc"} /> */}
-        <div className='flex flex-col justify-center text-center'>
-          <div className='m-20'>
-            <ButtonYellow>clubhouseやってます</ButtonYellow>
+        <div className="flex flex-col justify-center text-center">
+          <div className="m-20">
+            <ButtonYellow>
+              clubhouse、英系国際法律事務所にてRPAプロジェクト参戦中のためお休みしています
+            </ButtonYellow>
           </div>
           <a
-            href='https://apps.apple.com/jp/app/clubhouse-drop-in-audio-chat/id1503133294'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='inline-block mx-auto text-center rounded-full border-8 border-yellow-300 border-dotted'
+            href="https://apps.apple.com/jp/app/clubhouse-drop-in-audio-chat/id1503133294"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block mx-auto text-center rounded-full border-8 border-yellow-300 border-dotted"
           >
             <Image
-              className='bg-contain rounded-full border-8 border-yellow-300 border-dotted'
+              className="bg-contain rounded-full border-8 border-yellow-300 border-dotted"
               width={250}
               height={250}
               src={"/clubhouse.jpg"}
@@ -35,16 +37,16 @@ const language = () => {
           </a>
         </div>
 
-        <div className='flex flex-col justify-between text-center'>
-          <div className='p-10 md:p-20'>
-            <h3 className='mb-5 text-lg md:text-2xl underline'>
-              プログラミングの英語ドキュメンテーションやブログを音読
+        <div className="flex flex-col justify-between text-center">
+          <div className="p-10 md:p-20">
+            <h3 className="mb-5 text-lg md:text-2xl underline">
+              英語ドキュメンテーションやブログを音読
             </h3>
-            <p className='min-w-full text-sm md:text-lg lg:text-base leading-7 lg:leading-10 text-left '>
+            <p className="min-w-full text-sm md:text-lg lg:text-base leading-7 lg:leading-10 text-left ">
               平日の8:05-8:25
-              a.m.、徒然なるままに音読しています。React系のドキュメンテーションを読んでいることが多いです。ちょっと話題になったブログや、新しいPaasなどの情報も読んでます。
+              a.m.、徒然なるままにプログラミングの英語ドキュメンテーションやブログを音読しています。React系のドキュメンテーションを読んでいることが多いです。ちょっと話題になったブログや、新しいPaasなどの情報も読んでます。
             </p>
-            <p className='min-w-full text-sm md:text-lg lg:text-base leading-7 lg:leading-10 text-left '>
+            <p className="min-w-full text-sm md:text-lg lg:text-base leading-7 lg:leading-10 text-left ">
               もうすっかり廃れた感のあるclubhouseですが、訪れてくれる方がいて、継続する力になっています。感謝！
               使わなくなるとすぐに忘れてしまうのが、語学。プログラミングやってて、英語できてよかったなと思うこの頃です。
               言葉が読めるだけでは太刀打ちできませんが、日々小さな積み重ねで、瞬発力が出せると思っています。
@@ -59,7 +61,7 @@ const language = () => {
             Richmond, Surrey UK
           </span>
         </div> */}
-        <div className='mt-10 text-center'>
+        <div className="mt-10 text-center">
           <ImageShrink
             width={700}
             height={500}
